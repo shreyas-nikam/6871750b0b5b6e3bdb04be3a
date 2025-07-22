11bb7d3e873c916c377b1c802f1debd380b7be89
Shreyas Nikam <158344330+shreyas-nikam@users.noreply.github.com>
2025-07-22 16:42:49 -0400

    Add application_pages/page3.py

---
+++ application_pages/page3.py
@@ -0,0 +1,9 @@
+
+import streamlit as st
+
+def run_page3():
+    st.header("References")
+    st.markdown("""
+    [1] Chapter 3: The Risk Management Framework, Chapter 5: Risk Information, Operational Risk Manager Handbook, [Provided Document]. These sections discuss risk capacity, risk appetite, risk profile, and key risk indicators.
+    [2] dalmirPereira/module6_labWork: Lab Exercises from Module 6 - GitHub, https://github.com/dalmirPereira/module6_labWork.
+    """)
